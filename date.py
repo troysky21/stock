f442022f897995cdf71e641b3913134b744abc09
troysky21 <boreastroy@gmail.com>
2018-09-17 00:27:27 +0800

    date.py

---
+++ date.py
@@ -0,0 +1,10 @@
+import datetime
+
+
+numdays =20
+base = datetime.datetime.today()
+date_list = [base - datetime.timedelta(days=x) for x in range(0, numdays)]
+
+print(date_list[0].strftime('%Y%m%d') + 'a')
+
+x =1
